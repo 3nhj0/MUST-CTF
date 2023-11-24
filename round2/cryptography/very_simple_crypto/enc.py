a9db8a804ca3a9c27845dc4af8ca67c24628bce1
bilgee-cell <55517955+bilgee-cell@users.noreply.github.com>
2023-11-24 21:03:46 +0800

    Update enc.py

--- round2/cryptography/very_simple_crypto/enc.py
+++ round2/cryptography/very_simple_crypto/enc.py
@@ -1,12 +1,14 @@
 from flag import tug
 def encr(msg, x, y):
-    msg2 = int.from_bytes(msg.encode(), byteorder='big') #ene from_bytes zasah
+    msg2 = int.from_bytes(msg, byteorder='big')
     ct = pow(msg2, y, x)
     return ct
 
-til = 882564595536224140639625987659416029426239230804614613279163
-f4 = 65537 #chnge
-ctext = hex(encr(tug, til, f4))[2:]
+chosen = 65537
+seat_token = 882564595536224140639625987659416029426239230804614613279163
+encd = hex(encr(tug, seat_token, chosen))[2:]
 with open('ct.txt','w') as f:
-	f.write(ctext)
+	f.write(encd)
+print("your_ticket",encd)
 
+#your_ticket='0x8081d27d43aa21d16a5ecb683977c03df9f823fb737efd965f'
